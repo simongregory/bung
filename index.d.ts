90a771047e0ec4fa3db8b50e9c8d416873805d6d
Simon Gregory <simon.gregory@bbc.co.uk>
2019-11-28 17:14:05 +0000

    Add typescript definition

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export function fillCurlies(template:string, values:any): string;
+export function ropeyDateToEpoch(time:string): number;
+export function toJS(xml:string): JSON;
+export function getDayAbbrName(day: 0|1|2|3|4|5|6): string;
